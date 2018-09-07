3023d6422c96d16873e857d2a0d068feb6a31138
thescottbob <scottmcmains@gmail.com>
2018-09-06 23:07:53 -0700

    ScottsHW4

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,90 @@
-//Set the value of each crystal to a random number between 0 and 12 at the start of each round
+//Set the value of each crystal to a random number between 0 and 12 at the start of each round.
+//If random value = 0, then select a new random value. Log each value in the console for testing.
 
 var yellowCrystal = Math.floor(Math.random() * Math.floor(12));
+if (yellowCrystal === 0){
+    yellowCrystal = Math.floor(Math.random() * Math.floor(12));
+}
+console.log(yellowCrystal)
 
 var greenCrystal = Math.floor(Math.random() * Math.floor(12));
+if (greenCrystal === 0){
+    greenCrystal = Math.floor(Math.random() * Math.floor(12));
+}
+console.log(greenCrystal)
 
 var redCrystal = Math.floor(Math.random() * Math.floor(12));
+if (redCrystal === 0){
+    redCrystal = Math.floor(Math.random() * Math.floor(12));
+}
+console.log(redCrystal)
 
 var blueCrystal = Math.floor(Math.random() * Math.floor(12));
+if (blueCrystal === 0){
+    blueCrystal = Math.floor(Math.random() * Math.floor(12));
+}
+console.log(blueCrystal)
 
-console.log(yellowCrystal);
-console.log(greenCrystal);
-console.log(redCrystal);
-console.log(blueCrystal);
-
-//Set the value of the randomNum between 0 and 120 at the start of each round
+//Set the value of the randomNum between 0 and 120 at the start of each round. If random value is less than 12, then select a new random value. 
+//Log to the console for testing.
 
 var randomNum = Math.floor(Math.random() * Math.floor(120));
-
+if (randomNum < 12){
+  randomNum = Math.floor(Math.random() * Math.floor(120));
+}
 console.log(randomNum);
 
 //Set the initial totalScoreCount equal to 0
 
 var totalScoreCount= 0;
-var updatedScoreCount;
+var updatedScoreCount= [];
 
-//If user clicks on any of the four crystal buttons, then select the value of that crystal & add it to totalScoreCount
+//If user clicks on any of the four crystal buttons, then select the value of that crystal & add it to totalScoreCount,
+//which will be rendered to the page.
+
 $("#Yellowcrystal").on("click", function() {
-  totalScoreCount = totalScoreCount + yellowCrystal;
+  totalScoreCount += yellowCrystal;
   console.log(totalScoreCount);
   $("#totalScoreCount").html(totalScoreCount);
-  $(updatedScoreCount).push(totalScoreCount);
+  
+if (totalScoreCount == randomNum) {
+    console.log("first", first)
+    wins++;
+    $("#wins").html(wins);
+    //reset game;
+  }
+  else if(totalScoreCount > randomNum){
+      //alert("You lost");
+      console.log("working");
+  }
 });
 
+
 $("#Greencrystal").on("click", function() {
-    totalScoreCount = totalScoreCount + greenCrystal;
+    totalScoreCount += greenCrystal;
     console.log(totalScoreCount);
     $("#totalScoreCount").html(totalScoreCount);
+    $(updatedScoreCount).push(totalScoreCount);
   });
 
   $("#Redcrystal").on("click", function() {
-    totalScoreCount = totalScoreCount + redCrystal;
+    totalScoreCount += redCrystal;
     console.log(totalScoreCount);
     $("#totalScoreCount").html(totalScoreCount);
+    $(updatedScoreCount).push(totalScoreCount);
   });
 
   $("#Bluecrystal").on("click", function() {
-    totalScoreCount = totalScoreCount + blueCrystal;
+    totalScoreCount += blueCrystal;
     console.log(totalScoreCount);
     $("#totalScoreCount").html(totalScoreCount);
+    $(updatedScoreCount).push(totalScoreCount);
   });
 
-//Declaring two variable to track (and set the initial values of) the wins and losses of the user
+//Declaring two variables to track (and set the initial values of) the wins and losses of the user
 
-var wins= 0;
-var losses= 0;
-
+var wins= "0";
+var losses= "0";
 
 //If user wins, wins++ and reset game
 
